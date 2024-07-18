4d49da9f0b7d0363bd112d4bc5a1db874c72ed0b
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2024-07-18 10:59:54 -0300

    fiz o codigo, funciono, e depois do nada deu um erro de chave de array indefinida

--- assets/php/processa.php
+++ assets/php/processa.php
@@ -1,3 +1,39 @@
 <?php 
-    
+// configuração da DB
+$servername = "localhost"; // se fo em server coloca o ip
+$username = "root"; // coloca o usuario que vai te q loga no banco
+$password = ""; // se na instalação configuro uma senha coloca ela
+$dbname = "cadastro"; // nome do banco
+
+// criando conexão com a DB
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+// verificando a conexão
+if ($conn -> connect_error) {
+    die("Connection Failed: " . $conn -> connect_error);
+}
+
+// obtendo os dados do formulário
+$nome = $_POST['nome'];
+$data_nascimento = $_POST['data_nascimento'];
+$cpf = $_POST['cpf'];
+$email = $_POST['email'];
+$endereco = $_POST['endereco'];
+
+// preparando consulta('Querry');
+$sql = "INSERT INTO usuarios(nome,data_nascimento,cpf,email,endereco) VALUES (?,?,?,?,?)";
+$stmt = $conn -> prepare($sql);
+$stmt -> bind_param("sssss", $nome,$data_nascimento,$cpf,$email,$endereco);
+
+// executar a querry
+if ($stmt -> execute()) {
+    echo ("Cadastrado realizado com Sucesso!");
+} else {
+    echo ("Erro: " . $sql . "<br>" . $conn -> error);
+}
+
+// fechar a conexão
+$stmt -> close();  // no php a seta | -> | e para executar algo
+$conn -> close();
+
 ?>
--- index.php
+++ index.php
@@ -1,22 +1,54 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- links css -->
     <link rel="stylesheet" href="assets/css/index.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <!-- import bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- links php -->
     <?php 
         require 'assets/php/processa.php';
     ?>
     <title>Cadastro</title>
 </head>
-<body>
-    
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<body>
+
+    <h2>Formulário de Cadastro</h2>
+
+    <form action="assets/php/processa.php" method="post">
+
+        <label for="nome">Nome: </label><br>
+        <input type="text" name="nome" id="nome" require><br><br>
+
+        <label for="data_nascimento">Data de Nascimento: </label> <br>
+        <input type="date" name="data_nascimento" id="data_nascimento" require><br><br>
+
+        <label for="cpf">CPF: </label><br>
+        <input type="text" name="cpf" id="cpf" require><br><br>
+
+        <label for="email">Email: </label><br>
+        <input type="text" name="email" id="email" require><br><br> <!-- se de ruim troca o type de email pra text -->
+
+        <label for="endereco">Endereço: </label><br>
+        <input type="text" name="endereco" id="endereco" require><br><br>
+
+        <input type="submit" value="Cadastrar">
+
+    </form>
+
+
+
+
+    <!-- import bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
